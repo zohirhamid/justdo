07f3650f46d6fd8072b9f5af3da907ff3b9ecff3
zohir hamid <zohirhamid011@gmail.com>
2026-02-02 19:22:31 +0000

    update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 python manage.py migrate --noinput
 gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
+set -e
+
+cd backend
+python3 manage.py migrate --noinput
+python3 -m gunicorn config.wsgi:application --bind 0.0.0.0:$PORT
